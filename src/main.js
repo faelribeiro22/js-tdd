73850a174b28ec5fd980b70896d11aaf9d2e2b8e
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-11-26 17:39:06 -0200

    Add husky

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const a = 3;
+const b = 2;
+
+const c = 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasfasdfasfdasfdsadf';
+console.log('eiiii');
+function sum() {
+  return a + b + c;
+}
+sum();
