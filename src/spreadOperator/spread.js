314a5a11caab06e8d39f7ce74a46860295c132cc
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-12-18 14:36:08 -0200

    32. 8.2 - Usando o spread dentro de funções

--- src/spreadOperator/spread.js
+++ src/spreadOperator/spread.js
@@ -1,12 +1,20 @@
-let front = ['react', 'vue', 'angular'];
-let back = ['python', 'ruby', 'nodejs'];
+const front = ['react', 'vue', 'angular'];
+const back = ['python', 'ruby', 'nodejs'];
 
 console.log([...'Rafael']);
 console.log(...front);
 
 // before spread
 // let fullstack = front.concat(back);
-//console.log(fullstack);
+// console.log(fullstack);
 
-let fullstack = [...front, 'RxJS', ...back];
+const fullstack = [...front, 'RxJS', ...back];
 console.log(fullstack);
+
+// Spread em funções
+function makeSandwich(bread, cheese, sauce) {
+  console.log(`Ỳour Sandwich with ${bread} bread, ${cheese} cheese and ${sauce} is done!`);
+}
+
+const ingredients = ['white', 'cheddar', 'barbecue', 'teste'];
+makeSandwich(...ingredients);
