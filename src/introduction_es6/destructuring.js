059ea3b869b37bcf5c43705d0ceb96453756cf7a
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-12-18 14:04:12 -0200

    7.2 - Destructuring arrays

--- src/introduction_es6/destructuring.js
+++ src/introduction_es6/destructuring.js
@@ -17,3 +17,15 @@ console.log(github);
 const { github: gh } = data.social;
 
 console.log(gh);
+
+// Valor default caso não exista a propriedade no objeto
+const { city = 'Udia' } = data;
+console.log(city);
+console.log(data);
+
+//Destructuring array
+
+const array = ['Rafael', 'Ribeiro', 25, 'Udia'];
+
+const [firsName, secondName, age, cityAbbreviated] = array;
+console.log(firsName, secondName, age, cityAbbreviated);
