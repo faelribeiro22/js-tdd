955d62c9c06464fcb44fed0de408a8d334ebe579
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2017-12-19 00:37:13 -0200

    37. 10.1 - Herança Prototipal

---
+++ src/introduction_es6/heranca/herancaPrototipal.js
@@ -0,0 +1,11 @@
+function Animal(kind, sound) {
+  this.kind = kind;
+  this.sound = sound;
+}
+
+Animal.prototype.hello = function() {
+  console.log(`${this.sound} I'm a ${this.kind}!`);
+}
+const dog = new Animal('dog', 'sound');
+const cat = new Animal('cat', 'sound');
+dog.hello();
