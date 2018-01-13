4e47ef309483409b40c150c2b3d34eafed2df2c0
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2018-01-13 09:20:52 -0200

    Iniciando jshint

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-const a = 3;
-const b = 2;
 
-const c = 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasfasdfasdfasdfasdfasdfasfasdfasfdasfdsadf';
-console.log('eiiii');
-function sum() {
-  return a + b + c;
-}
-sum();
